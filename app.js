f766a1601c732c297a20df725d5afe6ad70bd305
sebastian <falconsebas23@gmail.com>
2026-03-26 18:49:25 -0500

    feat:ejercicio 5-tienda-de-productos

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ import { cajeroAutomatico } from './transferencia/eje4.js'
 import {  competenciaAtletismo } from './apropiacion/eje5.js'
 import {  cajeroAutomatico } from './transferencia/eje3.js'
 import { promedioNotas  } from './transferencia/eje4.js'
+import { tiendaProductos  } from './transferencia/eje5.js'
+
 
 multiposDe3();
 registrarIngresoEstudiantes();
