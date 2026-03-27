8ccf780f90dba594868ad63449893e8cdbd223ad
DAVID-17 <nlkonan20@gmail.com>
2026-03-27 11:10:15 -0500

    FEAT: Ejercicio 6–Juego del adivinador #19

---
+++ transferencia/eje6.js
@@ -0,0 +1,29 @@
+
+export function iniciarJuego() {
+    const numeroSecreto = Math.floor(Math.random() * 20) + 1;
+    let acertado = false;
+
+    console.log("Juego iniciado");
+
+    while (!acertado) {
+        let entrada = prompt("Adivina el número (1-20):");
+
+        if (entrada === null) break; // Cancelar juego
+
+        let intento = parseInt(entrada);
+
+        if (isNaN(intento)) {
+            alert("Ingresa un número válido.");
+            continue;
+        }
+
+        if (intento === numeroSecreto) {
+            alert("¡Felicidades! Adivinaste.");
+            acertado = true;
+        } else if (intento < numeroSecreto) {
+            alert("Es mayor que " + intento);
+        } else {
+            alert("Es menor que " + intento);
+        }
+    }
+}
